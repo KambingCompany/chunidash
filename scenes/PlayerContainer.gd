47bdd8421999bc1c39e279e1ab90033e323dfd91
Rendy Arya Kemal <renrror@gmail.com>
2024-04-24 19:13:13 +0700

    fix: use the correct size for bounds

--- scenes/PlayerContainer.gd
+++ scenes/PlayerContainer.gd
@@ -3,4 +3,4 @@ extends Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	find_child("CharacterController").bounds = get_size()
+	find_child("CharacterController").bounds = $Container.get_size()
