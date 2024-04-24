e94ca670e0de3606d3be8577e75670dd1c7de328
Adrian Ardizza <adrian.ardizza@gmail.com>
2024-04-24 15:22:49 +0700

    feat: implement combo and score based on osu!scorev1

--- scenes/HUDContainer.gd
+++ scenes/HUDContainer.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 var health: Label
 var score: Label
 var accuracy: Label
+var combo: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +11,7 @@ func _ready():
 	health = find_child("Health")
 	score = find_child("Score")
 	accuracy = find_child("Accuracy")
+	combo = find_child("Combo")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,6 +19,7 @@ func _process(delta):
 		health.text = "Health: %.1f" % GameState.health
 		score.text = "Score: %d" % GameState.score
 		accuracy.text = "Accuracy: %.2f" % (GameState.accuracy * 100)
+		combo.text = "Combo: %d" % GameState.combo
 
 func on_player_death():
 	health.text = "DEAD"
--- scenes/singletons/GameState.gd
+++ scenes/singletons/GameState.gd
@@ -11,6 +11,12 @@ enum Difficulty {
 	HARD
 }
 
+const DIFFICULTY_MULTIPLIERS = {
+	Difficulty.EASY: 0.75,
+	Difficulty.MEDIUM: 1.00,
+	Difficulty.HARD: 1.25,
+}
+
 # Set this before each level start based on player's diff choice
 var difficulty = Difficulty.EASY
 
@@ -24,6 +30,8 @@ var iframe = 0
 var colliding: bool
 
 var hit_count = 0
+var combo = 0
+
 # Judgements
 # TODO: Refactor (should this be separate variables or a dictionary?)
 var perfect = 0
@@ -35,21 +43,34 @@ func reset_game_state():
 	health = BASE_HEALTH
 	score = 0
 	alive = true
-	
+
+func _calculate_score(hit_value: int, combo: int, difficulty: Difficulty):
+	return hit_value * (1 + (clamp(combo - 1, 0, INF) * DIFFICULTY_MULTIPLIERS[difficulty]) / 25)
+
 func process_judgement(judgement: RhythmBar.Judgement):
 	match judgement:
 		RhythmBar.Judgement.PERFECT:
 			health = clamp(health + 1.0, 0, BASE_HEALTH)
+			score += _calculate_score(300, combo, difficulty)
+			
 			perfect += 1
+			combo += 1
 		RhythmBar.Judgement.GREAT:
 			health = clamp(health + 0.5, 0, BASE_HEALTH)
+			score += _calculate_score(100, combo, difficulty)
+
 			great += 1
+			combo += 1
 		RhythmBar.Judgement.GOOD:
 			health = clamp(health + 0.2, 0, BASE_HEALTH)
+			score += _calculate_score(50, combo, difficulty)
+			
 			good += 1
+			combo += 1
 		RhythmBar.Judgement.MISS:
 			health = clamp(health - 0.1, 0, BASE_HEALTH)
 			miss += 1
+			combo = 0
 
 	hit_count += 1
 	accuracy = (perfect + great * 0.75 + good * 0.5) / hit_count
