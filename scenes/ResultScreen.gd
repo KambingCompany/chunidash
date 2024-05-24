997ae7887eec8f6301ec281d5ad8efd9cd92ffad
Rendy Arya Kemal <renrror@gmail.com>
2024-05-24 20:24:49 +0700

    you win

--- scenes/ResultScreen.gd
+++ scenes/ResultScreen.gd
@@ -7,9 +7,11 @@ var great: Label
 var perfect: Label
 var combo: Label
 var hp: Label
+var song_finished: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	song_finished = find_child("SongFinished")
 	score = find_child("Score")
 	miss = find_child("Miss")
 	good = find_child("Good")
@@ -20,6 +22,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if GameState.health <= 0:
+		song_finished.text = "You Lose!"
+	elif GameState.boss_health <= 0:
+		song_finished.text = "You Win!"
 	score.text = "%010d" % GameState.score
 	miss.text = "Miss %d" % GameState.miss
 	good.text = "Good %d" % GameState.good
